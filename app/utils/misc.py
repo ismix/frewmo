0e06c1a1ca979b19e415b35974549b7202de7c7c
Ismail Coskuner <ismail@datascoreinc.com>
2016-06-01 15:40:52 -0700

    Recursive response formatter

--- app/utils/misc.py
+++ app/utils/misc.py
@@ -3,13 +3,32 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadSigna
 import flask
 
 
+def _resp_helper(item):
+    if hasattr(item, 'marshal_fields'):
+        return marshal(item, item.marshal_fields())
+
+    item_type = type(item)
+
+    if item_type == str:
+        return item
+
+    if item_type == dict:
+        for k, v in item.items():
+            item[k] = _resp_helper(v)
+        return item
+
+    try:
+        return [_resp_helper(i) for i in item]
+    except TypeError:
+        return item
+
+
 def resp(success, data=None, msg=""):
     if not data:
         data = {}
 
     for k, v in data.items():
-        if hasattr(v, 'marshal_fields'):
-            data[k] = marshal(v, v.marshal_fields())
+        data[k] = _resp_helper(v)
 
     return {'success': success, 'data': data, 'msg': msg}
 
